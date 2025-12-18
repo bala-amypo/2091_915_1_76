3d05f70af9651f29f6c4ea34709d364d511c6016
bala-amypo <bala@amypo.in>
2025-12-18 03:59:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/PersonProfile.java
+++ src/main/java/com/example/demo/entity/PersonProfile.java
@@ -1,5 +1,12 @@
 package com.example.demo.entity;
 
 public class PersonProfile{
-    private 
+    private Long id;
+    private String personType;
+    private String referenceId;
+    private String fullName;
+    private String email;
+    private String department;
+    private Boolean relationshipDeclared;
+    private LocalDateTime createdAt;
 }
