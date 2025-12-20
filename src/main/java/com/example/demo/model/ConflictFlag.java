b468d7466b763690e49a973ad6bf1ad85c6f1ea8
bala-amypo <bala@amypo.in>
2025-12-20 16:01:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/ConflictFlag.java
@@ -0,0 +1,66 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+
+@Entity
+public class ConflictFlag {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Long caseId;
+    private String flagType;
+    private String description;
+    private String severity;
+    private LocalDateTime flaggedAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getCaseId() {
+        return caseId;
+    }
+    public void setCaseId(Long caseId) {
+        this.caseId = caseId;
+    }
+    public String getFlagType() {
+        return flagType;
+    }
+    public void setFlagType(String flagType) {
+        this.flagType = flagType;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getSeverity() {
+        return severity;
+    }
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+    public LocalDateTime getFlaggedAt() {
+        return flaggedAt;
+    }
+    public void setFlaggedAt(LocalDateTime flaggedAt) {
+        this.flaggedAt = flaggedAt;
+    }
+    public ConflictFlag(Long id, Long caseId, String flagType, String description, String severity,
+            LocalDateTime flaggedAt) {
+        this.id = id;
+        this.caseId = caseId;
+        this.flagType = flagType;
+        this.description = description;
+        this.severity = severity;
+        this.flaggedAt = flaggedAt;
+    }
+    public ConflictFlag() {
+    }
+
+}
+
+
---
+++ src/main/java/com/example/demo/model/RelationshipDeclaration.java
@@ -0,0 +1,70 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+@Entity
+public class RelationshipDeclaration {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Long personId;
+    private String relatedPersonName;
+    private String relationshipType;
+    private String description;
+    private LocalDateTime declaredAt;
+    private Boolean isVerified;
+    public Long getId() {
+        return id;
+    }
+    public Long getPersonId() {
+        return personId;
+    }
+    public String getRelatedPersonName() {
+        return relatedPersonName;
+    }
+    public String getRelationshipType() {
+        return relationshipType;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public LocalDateTime getDeclaredAt() {
+        return declaredAt;
+    }
+    public Boolean getIsVerified() {
+        return isVerified;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+    public void setRelatedPersonName(String relatedPersonName) {
+        this.relatedPersonName = relatedPersonName;
+    }
+    public void setRelationshipType(String relationshipType) {
+        this.relationshipType = relationshipType;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setDeclaredAt(LocalDateTime declaredAt) {
+        this.declaredAt = declaredAt;
+    }
+    public void setIsVerified(Boolean isVerified) {
+        this.isVerified = isVerified;
+    }
+    public RelationshipDeclaration(Long id, Long personId, String relatedPersonName, String relationshipType, String description,
+            LocalDateTime declaredAt, Boolean isVerified) {
+        this.id = id;
+        this.personId = personId;
+        this.relatedPersonName = relatedPersonName;
+        this.relationshipType = relationshipType;
+        this.description = description;
+        this.declaredAt = declaredAt;
+        this.isVerified = isVerified;
+    }
+    public RelationshipDeclaration() {
+    }
+}
